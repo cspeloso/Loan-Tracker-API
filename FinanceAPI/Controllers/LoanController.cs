a253028589d1b0290cbde1402a5483730ce719af
chris <chris.peloso@atomri.com>
2025-12-08 18:37:31 -0500

    adding dedicated Loan controller

---
+++ FinanceAPI/Controllers/LoanController.cs
@@ -0,0 +1,43 @@
+namespace FinanceAPI.Controllers;
+
+using FinanceAPI.Models;
+using FinanceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("loans")]
+public class LoansController : ControllerBase
+{
+    private readonly FinanceDbContext _db;
+
+    public LoansController(FinanceDbContext db) => _db = db;
+
+    #region POSTs
+    [HttpPost]
+    public async Task<IActionResult> CreateLoan(CreateLoanRequest request)
+    {
+        var loan = new Loan
+        {
+            Name = request.Name,
+            Principal = request.Principal,
+            InterestRate = request.InterestRate,
+            TermInMonths = request.TermInMonths,
+            MonthlyPayment = request.MonthlyPayment,
+            StartDate = request.StartDate
+        };
+
+        _db.Loans.Add(loan);
+
+        await _db.SaveChangesAsync();
+
+        return Created($"/loans/{loan.Id}", loan);
+    }
+    #endregion POSTs
+
+    #region GETs
+    [HttpGet]
+    public async Task<List<Loan>> GetLoans() => await _db.Loans.ToListAsync();
+    #endregion GETs
+
+}
